e20967e40be7751dbe1f73b4295a0548afcbc1ac
Yash Patki <yashom.patki@gmail.com>
2024-10-22 15:57:10 -0700

    Changed main

--- unit5-cryptohustle/src/main.jsx
+++ unit5-cryptohustle/src/main.jsx
@@ -1,21 +1,27 @@
 // main.jsx
 import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import App from "./App.jsx";
 import Layout from "./routes/Layout";
 import DetailView from "./routes/DetailView";
 import NotFound from "./routes/NotFound";
+import "./index.css";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index={true} element={<App />} />
-                <Route path="/coinDetails/:symbol" element={<DetailView />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-        </Routes>
-    </BrowserRouter>,
-    document.getElementById("root")
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index={true} element={<App />} />
+                    <Route
+                        path="/coinDetails/:symbol"
+                        element={<DetailView />}
+                    />
+                </Route>
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>
 );
